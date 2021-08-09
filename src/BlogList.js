e559fb9d9a583dbf4bba610936170f8abce9991b
Cat <morancathy@gmail.com>
2021-08-08 21:42:47 -0500

    upates to blog

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+
+import {Link} from 'react-router-dom';
 function BlogList ({blogs, title, handleDelete}) {
 
   // const blogs = props.blogs;
@@ -8,8 +10,10 @@ function BlogList ({blogs, title, handleDelete}) {
     <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by: {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by: {blog.author}</p>
+          </Link>
           <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,15 @@ import BlogList from './BlogList';
 import useFetch from './useFetch';
 
 function Home () {
-  const {data: blogs, isPending, error} = useFetch(`http://localhost:8000/blogs`);
+  // const {data: blogs, isPending, error} = useFetch(`http://localhost:8000/blogs`);
   const [name, setName] = useState('Cathy');
   const [age, setAge] = useState('32')
   const [name1, setName1] = useState('mario')
-  // const [blogs, setBlogs] = useState(['']);
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
   const handleClick = (e) => {
     setName('Marta')
@@ -19,26 +23,26 @@ function Home () {
     console.log('hello ' + name , e.target)
   }
 
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter(blog => blog.id !== id)
-  //   setBlogs(newBlogs);
-  // }
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs);
+  }
 
 
 
   return (
     <div className="home">
-    {error && <div>{error}</div>}
+    {/*}{error && <div>{error}</div>}*/}
     <h2>Homepage</h2>
     <p>{name} is {age} years old.</p>
     <button onClick={handleClick}>Click Me</button>
     <button onClick={(e) => handleClickAgain('cathy', e)}>Click Me Again</button>
 
     <button onClick={()=> setName1('luigi')}>Change Name</button>
-    {isPending && <div> Loading ...</div>}
-    {/*blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>*/}  {/*property value*/}
-    {blogs && <BlogList blogs={blogs} title="All Blogs" />} 
-    {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />}
+    {/*{isPending && <div> Loading ...</div>}*/}
+    {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}  {/*property value*/}
+    {/*blogs && <BlogList blogs={blogs} title="All Blogs" />*/}
+    {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" handleDelete={handleDelete}/>}
 
     <p>{name1}</p>
     </div>
@@ -47,8 +51,3 @@ function Home () {
 }
 
 export default Home;
-
-
-// { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-// { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-// { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
